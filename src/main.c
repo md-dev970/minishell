50b8c0626a1af165560d46876f2e89b53f1d3a20
server_admin <md.dev970@outlook.com>
2025-01-27 22:02:51 +0000

    Modified lexer function : 	check for separators 	add tokens and separators (except for space) to a t_list and return it

--- src/main.c
+++ src/main.c
@@ -4,31 +4,86 @@
 #include <readline/readline.h>
 #include "../libft/include/libft.h"
 
-char *built_ins[7] = {"echo", "cd", "pwd", "env", "export", "unset", "exit"};
-char *separators[8] = {" ", ">", "<", "<<", ">>", "|", "\"", "'"};
+char *b[7] = {"echo", "cd", "pwd", "env", "export", "unset", "exit"};
+char *s[8] = {" ", ">", "<", "<<", ">>", "|", "\"", "'"};
 
-char **lexer(char* input)
+int is_separator(char *str, size_t i, size_t len)
+{
+        for (size_t j = 0; j < 8; ++j) {
+                if (str[i] == *s[j]) {
+                        if ((str[i] == '<' && i < len - 1 && str[i + 1] == '<')
+                        || (str[i] == '>' && i < len - 1 && str[i + 1] == '>'))
+                                return 2;
+                        else if (str[i] != ' ')
+                                return 1;
+                        else if (str[i] == '\"' || str[i] == '\'')
+                                return 0;
+                }
+                        
+        }
+        return -1;
+}
+
+t_list *lexer(char* input)
 {
         t_list *lst = NULL;
-        for(size_t i = 0; input[i]; ++i) {
-                for (size_t j = 0; j < 8; ++j) {
-                        // TODO : check for separators
-                        continue;
+        size_t len = ft_strlen(input);
+        size_t j;
+        char open_quote = '\0';
+        for(size_t i = 0; i < len; ++i) {
+                switch (input[i])
+                {
+                case ' ':
+                        break;
+                case '|':
+                        ft_lstadd_back(&lst, ft_lstnew(ft_substr(input, i, 1)));
+                        break;
+                case '<':
+                case '>':
+                        if (i == len - 1 || input[i + 1] != input[i]) {
+                                ft_lstadd_back(&lst, ft_lstnew(ft_substr(input, i, 1)));
+                                break;
+                        }
+                        ft_lstadd_back(&lst, ft_lstnew(ft_substr(input, i, 2)));
+                        i++;
+                        break;
+                case '\'':
+                case '\"':
+                        if (!open_quote)
+                                open_quote = input[i];
+                        else if (open_quote == input[i])
+                                open_quote = '\0';
+                        ft_lstadd_back(&lst, ft_lstnew(ft_substr(input, i, 1)));
+                        break;
+                default:
+                        j = i + 1;
+                        while (j < len && is_separator(input, j, len) == -1)
+                                j++;
+                        ft_lstadd_back(&lst, ft_lstnew(ft_substr(input, i, j - i)));
+                        i = j - 1;
+                        break;
                 }
         }
-        return ft_split(input, ' ');
+        return lst;
+}
+
+void print_lexems(void *lexems)
+{
+        char *s = (char *)lexems;
+        printf("%s\n", s);
 }
 
 void parser(char **lexems)
 {
-        printf("refuse\n");
+        /* TODO : parse the token list */
+        return;
 }
 
 int main()
 {
         int quit = 0;
         char *inputBuffer;
-        char **lexems;
+        t_list *lexems;
 
         while(quit == 0) {
 
@@ -38,15 +93,11 @@ int main()
 
                 if (strcmp(inputBuffer, "exit") == 0)
                         quit = 1;
-
-                for (size_t i = 0; lexems[i]; ++i) {
-                        printf("%s\n", lexems[i]);
-                        free(lexems[i]);
-                }
-
-                free(lexems);
+                free(inputBuffer);
+                ft_lstiter(lexems, &print_lexems);
+                ft_lstclear(lexems, &free);
         }
+        rl_clear_history();
 
-        free(inputBuffer);
         return 0;
 }
