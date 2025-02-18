5352ec9cb82da64db7a173f453b7f5629e884e8d
md-dev970 <md.dev970@outlook.com>
2025-02-18 18:02:03 +0000

    exec/update: update command arguments processing

--- src/main.c
+++ src/main.c
@@ -7,6 +7,12 @@
 #include "lexer.h"
 #include "parser.h"
 
+typedef struct args {
+        t_list *clargs;
+        t_list *hd;
+        t_list *inf;
+        t_list *ouf;
+} args;
 
 void print_tree(node *root)
 {
@@ -67,7 +73,7 @@ void execute(node *ast);
 
 void execute_pipe(node *ast, char *output);
 
-char **expand_input(node *ast);
+args *expand_input(node *ast);
 
 int main()
 {
@@ -106,12 +112,28 @@ void execute(node *ast)
                 return;
         
         printf("executing command %s \n", ast->left->value);
-        char **input = expand_input(ast->center);
+        args *ar = expand_input(ast);
+        char **input = (char **)malloc((ft_lstsize(ar->clargs) + 2) * sizeof(char *));
+        t_list *tmp = ar->clargs;
+        size_t i = 1;
+        printf("argument list size: %i\n", ft_lstsize(ar->clargs));
+        while (tmp) {
+                printf("arg: %s\n", (char *)tmp->content);
+                input[i++] = ft_strdup((char *)tmp->content);
+                tmp = tmp->next;
+        }
         if (!input) {
                 input = (char **)malloc(2 * sizeof(char *));
-                input[1] = NULL;
         }
-                
+        input[i] = NULL;
+        
+        ft_lstclear(ar->clargs, &free);
+        ft_lstclear(ar->hd, &free);
+        ft_lstclear(ar->inf, &free);
+        ft_lstclear(ar->ouf, &free);
+
+        free(ar);
+
         char *path = (char *)malloc(20 * sizeof(char));
         path[0] = '\0';
         ft_strlcat(path, "/bin/", 20);
@@ -177,7 +199,7 @@ void execute(node *ast)
         }
         
         
-        size_t i = 0;
+        i = 0;
         while (input && input[i]) {
                 printf("freeing input: %s\n", input[i]);
                 free(input[i]);
@@ -194,87 +216,54 @@ void execute(node *ast)
 
 void execute_pipe(node *ast, char *output)
 {
-        if (!ast)
-                return;
-        printf("executing command %s with input from %s\n", ast->left->value, output);
-        char **input = expand_input(ast->center);
-        size_t i = 0;
-        while (input && input[i])
-                free(input[i++]);
-        free(input);
-                
-        if (ast->right != NULL) {
-                printf("pipline\n");
-                execute_pipe(ast->right->center, ast->left->value);
-        }
-
+        return;
 }
 
-char *heredoc(char *delimiter)
+int *heredoc(char *delimiter)
 {
-        char *input = "";
-        size_t len = 0;
-        t_list *lines = NULL;
-        while (
-        ft_strlen(input) != ft_strlen(delimiter) ||
-        ft_strncmp(input, delimiter, ft_strlen(input)) != 0) {
+        char *input = readline(">");
+        int *p = (int *)malloc(2 * sizeof(int));
+        pipe(p);
+        while (ft_strlen(input) != ft_strlen(delimiter) ||
+                ft_strncmp(input, delimiter, ft_strlen(input)) != 0) {
                 
+                write(p[1], input, ft_strlen(input));
+                write(p[1], "\n", 1);
+                free(input);
                 input = readline(">");
-                len += ft_strlen(input) + 1;
-                ft_lstadd_back(&lines, ft_lstnew(input));
         }
-        len -= ft_strlen(delimiter) + 1;
-        input = (char *)malloc((len + 1) * sizeof(char));
-        if (!input)
-                return NULL;
-        input[0] = '\0';
-        t_list *tmp = lines;
-        while (tmp && tmp->next) {
-                ft_strlcat(input, (char *)tmp->content, len + 1);
-                ft_strlcat(input, "\n", len + 1);
-                tmp = tmp->next;
-        }
-        ft_lstclear(lines, &free);
-                
-        return input;
+        free(input);
+        close(p[1]);
+        return p;
 }
 
-void add_input(node *ast, t_list **input)
+void add_input(node *ast, args *input)
 {
         if (!ast) {
                 printf("null\n");
                 return;
         }
         if (ast->type == IDENT)
-                ft_lstadd_back(input, ft_lstnew(ft_strdup(ast->value)));
+                ft_lstadd_back(&(input->clargs), ft_lstnew(ft_strdup(ast->value)));
         if (ast->type == NONE && ast->left->type == DLT) {
                 printf("delimiter is: %s\n", ast->center->value);
-                char *text = heredoc(ast->center->value);
-                ft_lstadd_back(input, ft_lstnew(text));
+                ft_lstadd_back(&(input->clargs), ft_lstnew(heredoc(ast->center->value)));
                 return;
         }
         add_input(ast->left, input);
         add_input(ast->center, input);
 }
 
-char **expand_input(node *ast)
+args *expand_input(node *ast)
 {
         if (!ast)
                 return NULL;
-        
-        t_list *input = NULL;
-        add_input(ast, &input);
-        printf("----------------------\n Inputs: \n");
-        ft_lstiter(input, &print_lexem);
-        t_list *tmp = input;
-        printf("Input list size: %i\n", ft_lstsize(input));
-        char **args = (char **)malloc((ft_lstsize(input) + 2) * sizeof(char *));
-        for (size_t i = 1; i < ft_lstsize(input) + 1; ++i)
-        {
-                args[i] = ft_strdup((char *)tmp->content);
-                tmp = tmp->next;
-        }
-        args[ft_lstsize(input) + 1] = NULL;
-        ft_lstclear(input, &free);
-        return args;
+
+        args *a = (args *)malloc(sizeof(args));
+        a->clargs = NULL;
+        a->hd = NULL;
+        a->inf = NULL;
+        a->ouf = NULL;
+        add_input(ast, a);
+        return a;
 }
