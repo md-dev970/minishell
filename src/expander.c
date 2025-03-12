67c2e62d2ca1c13da1e755d96774307ad5915da4
md-dev970 <md.dev970@outlook.com>
2025-03-12 10:06:18 +0000

    fix(expander): Fix env vars expanding

--- src/expander.c
+++ src/expander.c
@@ -42,31 +42,42 @@ static char *expand_env(char *str, int last_exit_status)
                 }
                 size_t j = i;
                 if (q == '\'') {
-                        while (j < len && s[j] != q) {
+                        while (j < len && s[j] != q)
                                 j++;
-                        }
+
                         if (j < len)
                                 q = '\0';
+
                         if (j > i)
                                 ft_lstadd_back(&lst, ft_lstnew(ft_substr(s, i, j - i)));
+
                         i = j;
+
+
                 } else if (q == '\"') {
                         while (j < len && s[j] != q) {
-                                if (s[j] == '$') {
-                                        if (j > i)
-                                                ft_lstadd_back(&lst, ft_lstnew(ft_substr(s, i, j - i)));
-                                        i = j;
-                                        size_t k = j + 1;
-                                        while (k < len && s[k] != '=' && s[k] != '\'' && s[k] != ' ' && s[k] != '\"')
-                                                k++;
-                                        tmp = ft_substr(s, j + 1, k - j - 1);
-                                        if (k - j > 1)
-                                                ft_lstadd_back(&lst, ft_lstnew(ft_strdup(getenv(tmp))));
-                                        free(tmp);
-                                        j = k - 1;
-                                        i = j + 1;
+                                if (s[j] != '$') {
+                                        j++;
+                                        continue;
                                 }
-                                j++;
+                                        
+                                if (j > i)
+                                        ft_lstadd_back(&lst, ft_lstnew(ft_substr(s, i, j - i)));
+                                i = j;
+                                size_t k = j + 1;
+                                while (k < len && (ft_isalnum(s[k]) || s[k] == '_'))
+                                        k++;
+                                tmp = ft_substr(s, j + 1, k - j - 1);
+                                if (k - j > 1) {
+                                        if ((k - j) == 2 && *tmp == '?') {
+                                                ft_lstadd_back(&lst, ft_lstnew(ft_itoa(last_exit_status)));
+                                        } else {
+                                                ft_lstadd_back(&lst, ft_lstnew(ft_strdup(getenv(tmp))));
+                                        }
+                                }
+                                free(tmp);
+                                j = k;
+                                i = j;
                         }
                         if (j < len)
                                 q = '\0';
@@ -75,32 +86,34 @@ static char *expand_env(char *str, int last_exit_status)
                         i = j;
                 } else {
                         while (j < len && s[j] != '\'' && s[j] != '\"') {
-                                if (s[j] == '$') {
-                                        if (j > i)
-                                                ft_lstadd_back(&lst, ft_lstnew(ft_substr(s, i, j - i)));
-                                        i = j;
-                                        size_t k = j + 1;
-                                        while (k < len && s[k] != '=' && s[k] != '\'' && s[k] != ' ' && s[k] != '\"')
-                                                k++;
-                                        tmp = ft_substr(s, j + 1, k - j - 1);
-                                        if (k - j > 1) {
-                                                if ((k - j) == 2 && *tmp == '?') {
-                                                        ft_lstadd_back(&lst, ft_lstnew(ft_itoa(last_exit_status)));
-                                                } else {
-                                                        ft_lstadd_back(&lst, ft_lstnew(ft_strdup(getenv(tmp))));
-                                                }
-                                                
-                                        }
-                                                
-                                        free(tmp);
-                                        j = k + 1;
-                                        i = j;
+                                if (s[j] != '$') {
+                                        j++;
+                                        continue;
                                 }
-                                j++;
+                                        
+                                if (j > i)
+                                        ft_lstadd_back(&lst, ft_lstnew(ft_substr(s, i, j - i)));
+                                i = j;
+                                size_t k = j + 1;
+                                while (k < len && (ft_isalnum(s[k]) || s[k] == '_'))
+                                        k++;
+
+                                tmp = ft_substr(s, j + 1, k - j - 1);
+                                if (k - j > 1) {
+                                        if ((k - j) == 2 && *tmp == '?') {
+                                                ft_lstadd_back(&lst, ft_lstnew(ft_itoa(last_exit_status)));
+                                        } else {
+                                                ft_lstadd_back(&lst, ft_lstnew(ft_strdup(getenv(tmp))));
+                                        }
+                                }
+                                        
+                                free(tmp);
+                                j = k;
+                                i = k;
                         }
                         if (j > i)
                                 ft_lstadd_back(&lst, ft_lstnew(ft_substr(s, i, j - i)));
-                        i = j - 1;
+                        i = j;
                 }
         }
         #ifdef DEBUG
